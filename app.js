10d226924d1a74eefa4e56c5c97761c71c2aaa21
Theelipan Prabakar <theelipan@gmail.com>
2020-07-31 15:58:05 -0400

    Add transition to cart items

--- app.js
+++ app.js
@@ -40,36 +40,35 @@ let addToCartBtn = document.querySelectorAll(".card__addbtn");
         let currentItem_price =
           event.target.parentElement.children[2].innerText;
 
-        let inDiv = document.createElement("div");
-        inDiv.classList.add("shopping__cart_item");
-
+        let wrapperDiv = document.createElement("div");
         let itemCount = document.createElement("input");
+        let image = document.createElement("div");
+        let name = document.createElement("div");
+        let price = document.createElement("div");
+        let removebtn = document.createElement("div");
+
+        wrapperDiv.classList.add("shopping__cart_item");
+        image.classList.add("shopping__cart_item_image");
+        name.classList.add("item_name");
+        price.classList.add("item_price");
+        itemCount.classList.add("itemCount");
+        removebtn.classList.add("item_removebtn");
+
+        cart_btn.innerHTML = "<img src='/Images/full_cart2.PNG' />";
+        name.innerText = currentItem_name;
+        price.innerText = currentItem_price;
+        removebtn.innerText = "x";
         itemCount.setAttribute("type", "number");
         itemCount.setAttribute("min", "1");
-        itemCount.classList.add("itemCount");
         itemCount.setAttribute("value", 1);
 
-        let x = document.createElement("div");
-        x.classList.add("shopping__cart_item_image");
-        x.appendChild(currentItem_img.cloneNode(true));
-        inDiv.appendChild(x);
-
-        let y = document.createElement("div");
-        y.classList.add("item_name");
-        y.innerText = currentItem_name;
-        y.appendChild(itemCount);
-        inDiv.appendChild(y);
-
-        let z = document.createElement("div");
-        z.classList.add("item_price");
-        z.innerText = currentItem_price;
-        inDiv.appendChild(z);
-
-        let removebtn = document.createElement("div");
-        removebtn.classList.add("item_removebtn");
-        removebtn.innerText = "x";
-        // "<img src='https://img.icons8.com/dusk/64/000000/cancel.png' />";
-        y.appendChild(removebtn);
+        shopping_cart_item.appendChild(wrapperDiv);
+        image.appendChild(currentItem_img.cloneNode(true));
+        name.appendChild(removebtn);
+        name.appendChild(itemCount);
+        wrapperDiv.appendChild(image);
+        wrapperDiv.appendChild(name);
+        wrapperDiv.appendChild(price);
 
         // ---------------------------------------------------------------------
         itemCount.addEventListener("change", updateValue);
@@ -77,15 +76,13 @@ let addToCartBtn = document.querySelectorAll(".card__addbtn");
           let itemIncrease = e.target.value;
           let parsedPrice = parseFloat(currentItem_price.replace(/[$]+/g, ""));
           let updatedValue = (parsedPrice * itemIncrease).toFixed(2);
-          z.innerText = "$" + updatedValue;
+          price.innerText = "$" + updatedValue;
           showTotal();
           countItems();
         }
 
-        shopping_cart_item.appendChild(inDiv);
-        cart_btn.innerHTML = "<img src='/Images/full_cart2.PNG' />";
         cartStatus.style.display = "flex";
-        removeItem(removebtn, inDiv, this);
+        removeItem(removebtn, wrapperDiv, this);
         showTotal();
         countItems();
       }
@@ -119,19 +116,23 @@ function showTotal() {
 }
 
 // -----------------------------Remove item from cart-----------------------------------
-function removeItem(removebtn, inDiv, x) {
+function removeItem(removebtn, wrapperDiv, focus) {
   removebtn.addEventListener("click", () => {
-    inDiv.remove();
-    showTotal();
-    if (itemAdded === false) {
-      cart_empty.classList.remove("hidden");
-      cart_btn.innerHTML = "<img src='/Images/empty_cart2.PNG'/>";
-      cartStatus.style.display = "none";
-    } else if (itemAdded === true) {
-      cartStatus.style.display = "flex";
-    }
-    x.innerText = "Add to Cart";
-    countItems();
+    wrapperDiv.classList.add("animate");
+    setTimeout(function () {
+      wrapperDiv.remove();
+      showTotal();
+      if (itemAdded === false) {
+        cart_empty.classList.remove("hidden");
+        cart_btn.innerHTML = "<img src='/Images/empty_cart2.PNG'/>";
+        cartStatus.style.display = "none";
+      } else if (itemAdded === true) {
+        cartStatus.style.display = "flex";
+      }
+      focus.innerText = "Add to Cart";
+      countItems();
+    }, 500);
+    // wrapperDiv.remove();
   });
 }
 // -----------------------------showTotal----------------------------------------
